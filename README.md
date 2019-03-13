f47d44e12e95c81de441c76edbe2678cfdcfa380
touchDeeper <610690824@qq.com>
2019-03-13 11:13:08 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# Overview
+This is a persoal shared library include the commonly used functions in development.
+# Getting Started
+## Prerequisites
+### realsense
+- librealsense
+- OpenCV
+### threadSafeStructure
+- None
+
+##Install
+If you don't want to install the realsense module,jut comment the related line in the CMakelist.txt.
+```
+cmake .. -DCMAKE_INSTALL_PREFIX:=your_path
+make 
+make install
+```
+## Usage
+- test is used to test the TdLib.
+
+
--- src/realsense/realsense.h
+++ src/realsense/realsense.h
@@ -4,8 +4,8 @@
 
 #ifndef TDLIBRARY_REALSENSE_H
 #define TDLIBRARY_REALSENSE_H
-#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
-#include <librealsense2/rsutil.h>
+//#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+//#include <librealsense2/rsutil.h>
 #include <iostream>
 #include <vector>
 //PCL
@@ -17,12 +17,12 @@
 #include <opencv2/opencv.hpp>   // Include OpenCV API
 namespace td{
     namespace rs{
-        typedef pcl::PointXYZRGB RGB_Cloud;
-        typedef pcl::PointCloud<RGB_Cloud> point_cloud;
-        typedef point_cloud::Ptr cloud_pointer;
+//        typedef pcl::PointXYZRGB RGB_Cloud;
+//        typedef pcl::PointCloud<RGB_Cloud> point_cloud;
+//        typedef point_cloud::Ptr cloud_pointer;
         void helloz();
-        cloud_pointer PCL_Conversion_rect(const rs2::depth_frame& depth, const rs2::video_frame &color, const std::vector<cv::Point_<u_int32_t>> &vertex,const rs2_intrinsics intr_);
-        void getXYZfromUV(const rs2::depth_frame& frame,std::pair<int, int> u, float upoint[3],const rs2_intrinsics intr_);
+//        cloud_pointer PCL_Conversion_rect(const rs2::depth_frame& depth, const rs2::video_frame &color, const std::vector<cv::Point_<u_int32_t>> &vertex,const rs2_intrinsics intr_);
+//        void getXYZfromUV(const rs2::depth_frame& frame,std::pair<int, int> u, float upoint[3],const rs2_intrinsics intr_);
 
     }//namespace rs
 }//namespace td
