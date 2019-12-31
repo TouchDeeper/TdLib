20c5e9db16b9ca70e2322b96a5ac2f383f0d7719
touchDeeper <610690824@qq.com>
2019-12-31 22:06:28 +0800

    install directory

--- cmake_uninstall.cmake
+++ cmake_uninstall.cmake
@@ -8,7 +8,7 @@ foreach(file ${files})
   message(STATUS "Uninstalling $ENV{DESTDIR}${file}")
   if(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
     exec_program(
-      "/usr/local/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+      "/home/wang/下载/clion-2019.1.2/bin/cmake/linux/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
       OUTPUT_VARIABLE rm_out
       RETURN_VALUE rm_retval
       )
